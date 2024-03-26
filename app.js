bb2f4f3bbe58c73b8d045fccc75a8256fb514769
SteveJobs-2 <88567996+SteveJobs-2@users.noreply.github.com>
2024-03-26 16:55:20 +0800

    Modify app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/lostItemPosts', lostItemPostsRouter);
 
 let dataArray = [
       { name: "John", age: 12 },
